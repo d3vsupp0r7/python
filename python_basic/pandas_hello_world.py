f6f09b86e4d932bd8e20ec505deb9e7acdcd8477
d3vsupp0r7 <dev@dev.com>
2020-01-07 10:00:10 +0100

    First Commit

--- python_basic/pandas_hello_world.py
+++ python_basic/pandas_hello_world.py
@@ -1,3 +1,5 @@
+from operator import irshift
+
 import numpy as np
 import pandas as pd
 
@@ -48,3 +50,80 @@ print(dataFrameObject2)
 '''
 A Panel is a data structure that handles 3D data.
 '''
+
+
+# Working with files
+print('*** Pandas: Working with CSV files ***')
+## Iris dataset
+iris_pd_dataset = pd.read_csv("pandas_dataset_examples/iris.csv")
+print('** Pandas: HEAD')
+print('Using head as attribute will print all dataset')
+print(iris_pd_dataset.head)
+print('Show First 5 row (index 0->4).')
+print('REMEMBER: All row are numbered starting from 0 indexing: ')
+print(iris_pd_dataset.head())
+print('Show First 10 row')
+print(iris_pd_dataset.head(10))
+print('** Pandas: TAIL')
+print('Show Last 5 row (index 0->4).')
+print(iris_pd_dataset.tail())
+print('Show Last 10 row.')
+print(iris_pd_dataset.tail(10))
+
+print('** Pandas: CSV With no header')
+iris_pd_ds_no_header = pd.read_csv("pandas_dataset_examples/iris_noheader.csv")
+print(iris_pd_ds_no_header.head())
+print('Manually add header')
+print(' -) Manually add header: Headers < num_col')
+iris_pd_ds_no_header = pd.read_csv("pandas_dataset_examples/iris_noheader.csv", header=None, names=["h1","h2","h3","h4"])
+print(iris_pd_ds_no_header.head())
+print(' -) Manually add header: Headers = num_col')
+iris_pd_ds_no_header = pd.read_csv("pandas_dataset_examples/iris_noheader.csv", header=None, names=["h1","h2","h3","h4","h5"])
+print(iris_pd_ds_no_header.head())
+print(' -) Manually add header: Headers > num_col')
+iris_pd_ds_no_header = pd.read_csv("pandas_dataset_examples/iris_noheader.csv", header=None, names=["h1","h2","h3","h4","h5","h6"])
+print(iris_pd_ds_no_header.head())
+
+print('** Pandas: Columns')
+print(iris_pd_dataset.columns)
+
+print('** Pandas: Info on columns type of dataset')
+print(' -) info() as a function -> get type of columns')
+print(iris_pd_dataset.info())
+print(' -) info as a attribute -> print a dataset')
+print(iris_pd_dataset.info)
+
+print('** Pandas: Working with datasets')
+print(' -) Get all column values of: sepal_length')
+Y =  iris_pd_dataset['sepal_length']
+print(Y)
+print(' -) Get all column values of: sepal_length.\n Col name as string variable.')
+col_name = "sepal_length";
+Y =  iris_pd_dataset[col_name]
+print(Y.head())
+print(' -) Get subset of columns.')
+Y_subset_cols = iris_pd_dataset[["sepal_length","sepal_width","petal_length"]]
+print(Y_subset_cols.head())
+col_array = ["sepal_length","sepal_width","petal_length"]
+print(' -) Get subset of columns.\n Col names as string array.')
+Y_subset_cols_str = iris_pd_dataset[col_array]
+print(Y_subset_cols_str.head())
+print(' *) NOTE IF WE SELECT A SUBSET OF COLUMNS, IT''S TYPE. IS A DataFrame')
+
+# A Single column of dataset in panda is a "Series" object
+print(' -) Get types of objects')
+print('Y is of type: ' + str(type(Y)) )
+print('Y_subset_cols is of type: ' + str(type(Y_subset_cols)) )
+print('iris_pd_dataset is of type: ' + str(type(iris_pd_dataset)) )
+
+print(' -) REMOVE A COLUMN From Dataset: drop()')
+# drop require the axis parameter
+# IMPO: The axis parameter
+Y_col_rem = iris_pd_dataset.drop("species", axis=1)
+print(Y_col_rem.head())
+
+print(' -) Remove a row by index. Remove the first row: index[0]')
+Y_drop_sample = iris_pd_dataset.head()
+print(Y_drop_sample)
+drop_out = Y_drop_sample.drop(Y_drop_sample.index[0])
+print(drop_out)
