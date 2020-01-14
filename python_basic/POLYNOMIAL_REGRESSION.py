c49caba286c30b714813eaf6695c0d31bee2ec52
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2020-01-14 17:35:18 +0100

    First commit

--- python_basic/POLYNOMIAL_REGRESSION.py
+++ python_basic/POLYNOMIAL_REGRESSION.py
@@ -31,3 +31,81 @@ print(pd_dataset.head())
 cols = ["RM","LSTAT","DIS","RAD","MEDV"]
 sns.pairplot(pd_dataset[cols])
 plt.show()
+
+# Create numpy arrays
+X = pd_dataset[["LSTAT"]].values
+Y = pd_dataset["MEDV"].values
+
+# Create set for train/test
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3, random_state=0)
+
+print('DS subdivision: ')
+print('*** X_train ***')
+print('X_train shape: ' + str(X_train.shape) )
+print('X_train sample data ')
+print(X_train[:5])
+#
+print('*** X_test ***')
+print('X_test shape: ' + str(X_test.shape) )
+print('X_test sample data ')
+print(X_test[:5])
+#
+print('*** Y_train ***')
+print('Y_train shape: ' + str(Y_train.shape) )
+print('Y_train sample data ')
+print(Y_train[:5])
+#
+print('*** Y_test ***')
+print('Y_test shape: ' + str(Y_test.shape) )
+print('Y_test sample data ')
+print(Y_test[:5])
+
+## POLYNOMIAL ANALYSIS ##
+from sklearn.preprocessing import PolynomialFeatures
+polynomial_degree = 2
+polyfeats = PolynomialFeatures(degree=polynomial_degree)
+
+# Build the polinomyal feature
+X_train_poly = polyfeats.fit_transform(X_train)
+X_test_poly = polyfeats.fit_transform(X_test)
+
+print('*** Compare train sets ***')
+print('-) Normal train set')
+print(X_train[:5])
+print('-) Polynomial train sets')
+'''
+IMPO: using the polinomyal approach, make the set composet of 
+degrees+1 columns.
+
+If degree was 2, we have 3 columns
+-) first column is the feature raised to 0 (so it gives always 1 as result)
+-) second column is the feature raised to 1 (so it gives the number itself)
+-) third column is the feature raised to 2 (the degree we have specified)
+'''
+print(X_train_poly[:5])
+
+#degree comparison
+for i in range(1,11):
+    polyfeats = PolynomialFeatures(degree=i)
+    X_train_poly = polyfeats.fit_transform(X_train)
+    X_test_poly = polyfeats.fit_transform(X_test)
+    #
+    ll = LinearRegression()
+    ll.fit(X_train_poly,Y_train)
+    Y_pred = ll.predict(X_test_poly)
+    #
+    print(' *************** ')
+    print('Degree: ' + str(i))
+    print('Prediction output sample')
+    print(Y_pred[:5])
+    #
+    MSE = mean_squared_error(Y_test, Y_pred)
+    r2_score_out = r2_score(Y_test, Y_pred)
+    print("MSE: " + str(MSE))
+    print("R2 score: " + str(r2_score_out))
+    # Plot LinearRegression Coefficient
+    weigth_of_linearRegression = ll.coef_
+    print('Weigth of calculated linearRegression: ' + str(weigth_of_linearRegression))
+    bias_or_intercept_of_linearRegression = ll.intercept_
+    print('Bias of calculated linearRegression: ' + str(bias_or_intercept_of_linearRegression))
+    print(' *************** ')
