6f394d84e6f6074460f271d14ba872b7cbc0bb81
Aniello La Bua <aniello.labua@alliance-healthcare.net>
2020-01-21 17:32:53 +0100

    First commit

--- python_basic/MULTICLASS_CLASSIFICATION.py
+++ python_basic/MULTICLASS_CLASSIFICATION.py
@@ -10,30 +10,98 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+from sklearn.metrics import accuracy_score
 
-ds_filename = "pandas_dataset_examples/wdbc.data"
+from sklearn.datasets import load_digits
+'''
+This example uses the sklearn digit dataset to exlore the multiclass classification problem.
+This dataset represent the all digits,0 to 9, digitally scanned.
+The objective is to try the right classification for the images, that are rapresented as pixel values on 
+gray scale from 0 to 255.
+'''
+enable_digit_printing = "n"
 
-pd_dataset = pd.read_csv(ds_filename,names=["id","diagnosis","radius_mean","texture_mean","perimeter_mean","area_mean",
-                                            "smoothness_mean","compactness_mean","concavity_mean","concave points_mean",
-                                            "symmetry_mean","fractal_dimension_mean","radius_se","texture_se",
-                                            "perimeter_se","area_se","smoothness_se","compactness_se","concavity_se",
-                                            "concave points_se","symmetry_se","fractal_dimension_se","radius_worst",
-                                            "texture_worst","perimeter_worst","area_worst","smoothness_worst",
-                                            "compactness_worst","concavity_worst","concave points_worst",
-                                            "symmetry_worst","fractal_dimension_worst"])
+pd_dataset = load_digits()
+
+X = pd_dataset.data
+Y = pd_dataset.target
 
 print('*) DS Info')
-print(pd_dataset.info())
 print('-) DS shape')
-print(pd_dataset.shape)
-print('-) DS describe()')
-print(pd_dataset.describe())
-print('-) DS sample data')
-print(pd_dataset.head())
-print('-) DS correlation information')
-from tabulate import tabulate
-print(tabulate(pd_dataset.corr()))
+print(X.shape)
+print('-) DS classes')
+print(np.unique(Y))
 
-print('* 1. Determinate list of classes to predict')
-class_column = "diagnosis"
-print(pd_dataset[class_column].unique() )
+# print the example images
+if enable_digit_printing.lower() in ['y', 'yes']:
+    for i in range(0,10):
+        '''
+        '''
+        fig = plt.figure(figsize=(12,10) )
+        pic_matrix = X[Y==i][0].reshape([8,8])
+        plt.imshow(pic_matrix, cmap="gray")
+        fig.suptitle('Image of number: ' + str(i), fontsize=20)
+        plt.show()
+
+# Create set for train and test
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.3, random_state=0)
+'''
+Normalize columns
+'''
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train = mms.fit_transform(X_train)
+X_test = mms.transform(X_test)
+
+'''
+Apply the model for multiclass classification.
+By default the sklearn library can manage the multiclass classification without any additional parameters.
+
+'''
+
+lr = LogisticRegression()
+lr.fit(X_train,Y_train)
+
+Y_pred = lr.predict(X_test)
+Y_pred_proba = lr.predict_proba(X_test)
+
+print('*** Calculate the performance of models')
+accuracy = accuracy_score(Y_test, Y_pred)
+print('Accuracy: ' + str(accuracy))
+
+logLoss = log_loss(Y_test, Y_pred_proba)
+print('Log Loss: ' + str(logLoss))
+
+# Matrix of confusion
+print('*** Print Confusion Matrix')
+from sklearn.metrics import confusion_matrix
+'''
+How to read confusion matrix?
+
+cols: predicted class
+rows: correct class
+
+'''
+cm = confusion_matrix(Y_test,Y_pred)
+from tabulate import tabulate
+print(tabulate(cm))
+
+## Using seaborn to plot confusion matrix
+import seaborn as sns
+
+plt.figure(figsize=(9,9))
+sns.heatmap(cm,annot=True)
+plt.ylabel("Correct class")
+plt.xlabel("Predicted class")
+plt.show()
+
+'''
+Optimize the heatmap graph drawing
+'''
+plt.figure(figsize=(9,9))
+sns.heatmap(cm,annot=True, cmap="Blues_r", linewidths=.5, square=True)
+plt.ylabel("Correct class")
+plt.xlabel("Predicted class")
+plt.show()
