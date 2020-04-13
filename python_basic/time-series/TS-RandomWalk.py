09fb2a1aab3e52ec018348ec046555aaa1138585
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-04-13 14:35:27 +0200

    First commit

--- python_basic/time-series/TS-RandomWalk.py
+++ python_basic/time-series/TS-RandomWalk.py
@@ -8,11 +8,23 @@ import statsmodels.tsa.stattools as sts
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 #
-ds_filename = "../time-series_datasets/market_index_2018.csv"
+ds_filename = "../time-series_datasets/market_index_2018_RandWalk_example.csv"
 
 #Define the complete dataframe
 pd_dataset_compl = pd.read_csv(ds_filename)
 # working dataset: pd_dataset
 pd_dataset = pd.read_csv(ds_filename)
 
+# Working with dataset
+rw=pd_dataset.copy()
+rw.date = pd.to_datetime(rw.date, dayfirst = True)
+rw.set_index("date", inplace=True)
+rw=rw.asfreq('b')
+#
+size = int(len(rw)*0.8)
+
+print("rw describe()")
+print(rw.describe())
+print("rw head()")
+print(rw.head())
 #
