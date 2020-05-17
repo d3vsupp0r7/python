07afb9be3066772833d20da213a44a3a73f65d83
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-05-17 12:13:01 +0200

    First commit

--- Python_NLP/4_Text_Preprocessing.py
+++ Python_NLP/4_Text_Preprocessing.py
@@ -53,3 +53,56 @@ print(nltk_ita_tokens)
 print("     *) ENG NLTK TOKEN **")
 print(nltk_eng_tokens)
 
+#
+print('*) Evaluate ENG complex phrase')
+eng_textToAnalize_02 = ("I'd love to visit U.S.A, in particular i want to run with a motorcycle on Route 66 making a classic trip on the road. "
+"I'd like to live the emotions like the Easy Rider film, watching the wild nature. But... i'cant. What a nice experience it would be!!!!")
+print(eng_textToAnalize_02)
+print(" ** NLTK TOKEN TYPE **")
+nltk_eng_tokens = word_tokenize(eng_textToAnalize_02)
+print("*) Tokens type: ")
+print(type(nltk_eng_tokens))
+print("*)Tokens of string: " + str(len(nltk_eng_tokens)) )
+print("*)Tokens printed: ")
+print(nltk_eng_tokens)
+
+#Iterate over tuples
+'''for var in nltk_eng_tokens:
+    print ("["+str(nltk_eng_tokens.index(var)) + "]",var)
+'''
+
+#Iterate over list
+print("*)Tokens printed and formatted: ")
+for i in range(len(nltk_eng_tokens)):
+    print("Token [{}]: {}".format(i + 1, nltk_eng_tokens[i]))
+
+#NLTK: tokenize in phrases
+print(" ** NLTK TOKENS IN PHRASES TYPE **")
+from nltk.tokenize import sent_tokenize
+#5 propositions [?]
+print("*) Original Phrase")
+print(eng_textToAnalize_02)
+'''
+Token [1]: I'd love to visit U.S.A, in particular i want to run with a motorcycle on Route 66 making a classic trip on the road.
+Token [2]: I'd like to live the emotions like the Easy Rider film, watching the wild nature.
+Token [3]: But... i'cant.
+Token [4]: What a nice experience it would be!!!
+Token [5]: !
+'''
+nltk_eng_phrases_tokens = sent_tokenize(eng_textToAnalize_02)
+print(type(nltk_eng_phrases_tokens))
+print("*)Tokens of string: " + str(len(nltk_eng_phrases_tokens)) )
+print("*)Tokens printed: ")
+print(nltk_eng_phrases_tokens)
+print("*)Tokens printed and formatted: ")
+#
+for i in range(len(nltk_eng_phrases_tokens)):
+    print("Token [{}]: {}".format(i + 1, nltk_eng_phrases_tokens[i]))
+
+## STOP WORDS ##
+'''
+Stop word are common use words that they do not bring any useful information to the text
+Stop words are:
+*) congiunction:
+
+'''
